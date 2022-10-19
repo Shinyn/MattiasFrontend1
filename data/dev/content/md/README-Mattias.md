e3b4436c7e6907c0179e73ddf697ad5895d07978
Shinyn <mattias.alm@hotmail.com>
2022-10-19 18:43:05 +0200

    Renamed some files, updated README-Mattias.md file, added several functions and comments

---
+++ data/dev/content/md/README-Mattias.md
@@ -0,0 +1,70 @@
+# Make Groups (Examining assignment for frontend 1)
+
+---
+
+## Instructions for installing & running the program:
+
+To clone and run this program you'll need Git and Node.js installed.
+
+- Clone repo
+- npm install
+- npm run init (OPTIONAL) - If you want to add your own information
+- npm start
+- Click "Mattias"
+- Chose parameter from the dropdown menu
+- Click "Submit"
+- Click "Visa grupp"
+
+---
+
+## Purpose of the program:
+
+The program sorts and displays JSON files based on a parameter the user chooses from the dropdown menu.
+
+### Simpler:
+
+Takes info, sorts it into groups and displays it on a webpage.
+
+---
+
+### This program exist as a personal learning tool for coding
+
+It also serves the function of being the examining assignment for the frontend 1 course.  
+Other than those 2 reasons it exists to sort and print out information from JSON objects.
+
+---
+
+#Technical documentation:
+
+## **My solution**:
+
+1. Use fetch() to access the JSON files on the local server
+2. Use forEach() to go through all files and use destructuring to access the key / value pair of each individual file
+3. Use a function that sorts the files into groups based on one of the key's from the JSON file
+4. Use document.getelementById() to connect a button in html with js so that when you press the button
+
+   3.2 A step by step guide to using the code you have written.
+   3.3 All technical documentation should fit into README.md.
+   Advanced requirements
+   3.3 Describe more than one way of reusing your solution.
+   3.4 Make your code as easy as possible to reuse by someone else.
+   Show by example how you have taken into account who else might
+   want to solve your particular problem, as well as when and why
+
+## If you want to use the code:
+
+- [ ] Skriv om vad man kan göra för att tweeka programmet så att man kan använda sig av alla parametrar i json filerna, samt vad man behöver lägga till för att visa dom i html'en.
+
+---
+
+## Contribution
+
+The bulk of the code is written by my teacher _Peter Karlsson_ a.k.a. _rymdpeter_
+
+```
+Code written by me:
+- mattias.html
+- mattias.js
+- mattias.css
+- README-Mattias.md (This file)
+```
--- www/dev/mattias.js
+++ www/dev/mattias.js
@@ -1,7 +1,16 @@
 "use strict";
+// Kopplar ihop listan, formuläret och knapparna i html med denna js fil
 const list = document.getElementById("list");
 const clear = document.getElementById("clear");
 const form = document.getElementById("makeGroups");
+const showGroupButton = document.getElementById("showGroup");
+let greenGroup = [];
+let redGroup = [];
+let blueGroup = [];
+let yellowGroup = [];
+let firstNames = [];
+let lastNames = [];
+let hashGroup = [];
 
 // Från html så hämtar vi form som är en dropdown meny där vi kan välja vilken parameter vi vill sortera / gruppera efter.
 // På form sätter vi en eventListener som aktiverar handleform funktionen när vi clickar på submit knappen.
@@ -19,9 +28,37 @@ function handleForm(event) {
   getInfo(result);
 }
 
+// När man klickar på Visa Grupp knappen så körs showGroup funktionen
+showGroupButton.addEventListener("click", showGroup);
+// Rensar först listan / listorna och kallar sen på funktionen "printGroupMembers"
+function showGroup() {
+  clearList();
+  let value = document.getElementById("groupAs").value;
+  if (value === "firstname") {
+    console.log(firstNames.sort());
+  }
+  if (value === "lastname") {
+    console.log(lastNames.sort());
+  }
+  if (value === "hash") {
+    hashGroup.forEach((element) => {
+      element.filter((word) => word.split(""));
+    });
+    console.log(hashGroup.sort());
+  }
+  if (value === "personalityType") {
+    printGroupMembers(greenGroup);
+    printGroupMembers(yellowGroup);
+    printGroupMembers(redGroup);
+    printGroupMembers(blueGroup);
+  }
+}
+
+// Sorterar det som skickas in i alfabetisk ordning
+
 // Här nedanför hämtar vi alla users från addressen "http://localhost:3000/api/users" genom att klicka på knappen "submit" (beskrivs ovan).
 // När man trycker på knappen hämtas users med fetch. Svaret vi får (res) konverteras till ett json objekt.
-// Sen loopar vi igenom alla json filer med forEach() och använder oss av destrcturing för att plocka ut valda parametrar.
+// Sen loopar vi igenom alla json filer med forEach() och använder oss av destrcturing för att plocka ut samtliga parametrar.
 //
 // Eftersom inte alla users har birthYear och zodiac som parametrar så sätter vi
 // deras default värden till "no birthYear given" och "no zodiac given" så att man
@@ -56,9 +93,11 @@ function getInfo(tmp) {
 
         const li = document.createElement("li");
         if (tmp === "firstname") {
+          firstNames.push(firstname);
           li.innerText = `${firstname}`;
         }
         if (tmp === "lastname") {
+          lastNames.push(lastname);
           li.innerText = `${lastname}`;
         }
         if (tmp === "discord") {
@@ -71,9 +110,11 @@ function getInfo(tmp) {
           li.innerText = `${skills}`;
         }
         if (tmp === "personalityType") {
+          sortByPersonalityType(personalityType, item);
           li.innerText = `${personalityType}`;
         }
         if (tmp === "hash") {
+          hashGroup.push(hash);
           li.innerText = `${hash}`;
         }
         if (tmp === "belongsTo") {
@@ -87,10 +128,41 @@ function getInfo(tmp) {
     });
 }
 
-// Rensar all data som visas i listan (tar bort alla child element i ul taggen)
+// När man klickar på clear knappen så körs clearList funktionen
 clear.addEventListener("click", clearList);
+// Rensar all data som visas i listan (tar bort alla child element i ul taggen)
+// FIXME:TODO: Dessa kan skriva till samma lista istället för 2 olika då ingen text ska visas samtidigt som någon annan
 function clearList() {
   while (list.firstChild) {
     list.removeChild(list.firstChild);
   }
+}
+
+//TODO: Skriv mer utförligt
+// Skriver ut samtliga medlemmar i sina respektive färg-grupper och visar dom på hemsidan
+function printGroupMembers(group) {
+  for (const person of group) {
+    let li = document.createElement("li");
+    li.innerText = `People in group ${person.personalityType}: ${person.firstname}`;
+    list.appendChild(li);
+  }
+}
+
+function sortByChars(arr) {
+  const regExA = /([A-F])\w+/g;
+  const regExG = /([G-M])\w+/g;
+  const regExN = /([N-Ö])\w+/g;
+
+  // Tar in array med personer som ska grupperas (dom har redan blivit sorterade vid dehär laget
+  // använd RegEx för att matcha i olika grupper
+  console.log(arr + "<- Arr");
+}
+sortByChars(firstNames);
+
+// Sorterar in personer i grupper baserat på färg (persontyp)
+function sortByPersonalityType(color, personObject) {
+  if (color === "grön") greenGroup.push(personObject);
+  if (color === "röd") redGroup.push(personObject);
+  if (color === "blå") blueGroup.push(personObject);
+  if (color === "gul") yellowGroup.push(personObject);
 }
